48e540b61984691febee6e9ef72c49c7b716158e
Paul Daniel Faria <Nashenas88@users.noreply.github.com>
2020-03-03 22:52:15 -0500

    Add macro to simplify using closures as providers

--- coi-test/tests/register_closure.rs
+++ coi-test/tests/register_closure.rs
@@ -1,4 +1,4 @@
-use coi::{container, Container, Inject};
+use coi::{container, provide_closure, Inject};
 use std::sync::Arc;
 
 pub trait Trait1: Inject {}
@@ -13,11 +13,8 @@ struct Impl {
 }
 
 impl Impl {
-    fn new(trait2: Arc<dyn Trait2>, data: String) -> Self{
-        Self {
-            trait2,
-            data
-        }
+    fn new(trait2: Arc<dyn Trait2>, data: String) -> Self {
+        Self { trait2, data }
     }
 }
 
@@ -32,12 +29,14 @@ impl Trait2 for Impl2 {}
 #[test]
 fn main() {
     let x = String::from("3");
+    let trait1_provider = provide_closure!(
+        move |trait2: Arc<dyn Trait2>| -> coi::Result<Arc<dyn Trait1>> {
+            Ok(Arc::new(Impl::new(trait2, x.clone())) as Arc<dyn Trait1>)
+        }
+    );
     let container = container! {
         trait2 => Impl2Provider,
-        trait1 => move |container: &Container| -> coi::Result<Arc<dyn Trait1>> {
-            let trait2 = container.resolve::<dyn Trait2>("trait2")?;
-            Ok(Arc::new(Impl::new(trait2, x.clone())) as Arc<dyn Trait1>)
-        }; scoped,
+        trait1 => trait1_provider; scoped,
     };
     let _trait1 = container
         .resolve::<dyn Trait1>("trait1")
